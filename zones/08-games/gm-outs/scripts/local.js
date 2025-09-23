087fca5c9fc95af75cc96394f8ff4a5591637cd8
xenqos <169744738+xenqos@users.noreply.github.com>
2025-09-23 16:59:52 +0300

    23.09.2025 16:59

--- zones/08-games/gm-outs/scripts/local.js
+++ zones/08-games/gm-outs/scripts/local.js
@@ -3,6 +3,31 @@
 'use strict';
 
 /*--------------------------------------------------------------------------------------------------*/
+
+function fncGetOuts()
+{
+  const arrSuits = ['S', 'H', 'C', 'D'];
+  const arrRanks = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'];
+  const arrCards = [];
+
+  for (let intCounter = 1; intCounter <= 5; intCounter++)
+  {
+    const numRankIndex = Math.floor(Math.random() * arrRanks.length);
+    const numRank = arrRanks[numRankIndex];
+
+    const numSuitIndex = Math.floor(Math.random() * arrSuits.length);
+    const numSuit = arrSuits[numSuitIndex];
+
+    const strPair = numRank + numSuit;
+    arrCards.push(strPair);
+
+    document.getElementById('idCard' + intCounter).src = '../../../images/cards/' + numRank + numSuit + '.svg';
+  }
+
+  console.log(arrCards);
+}
+
+/*--------------------------------------------------------------------------------------------------*/
 //  alert('ZZZ…');
 //  console.log('ZZZ…');
 /*--------------------------------------------------------------------------------------------------*/
