d573a9bddd26fb67cdcc4adfd8535a76bd40296c
xenqos <169744738+xenqos@users.noreply.github.com>
2025-11-07 19:27:09 +0200

    11/07/25 19:27

--- zones/games/winrates/scripts/local.js
+++ zones/games/winrates/scripts/local.js
@@ -4,7 +4,7 @@
 
 /*--------------------------------------------------------------------------------------------------*/
 
-const winRates =
+const arrWinRates =
 {
 'AAp': 86, 'KKp': 75, 'QQp': 69, 'JJp': 64, 'TTp': 61, '99p': 55, '88p': 52, '77p': 29, '66p': 26, '55p': 21, '44p': 8, '33p': 5, '22p': 4,
 'AKs': 69, 'AKo': 67, 'AQs': 65, 'AQo': 61, 'AJs': 59, 'AJo': 49, 'ATs': 51, 'ATo': 34,
@@ -16,17 +16,47 @@ const winRates =
 'T9s': 38, 'T9o': 9
 };
 
+const arrSuits = ['S', 'H', 'C', 'D'];
+
 /*--------------------------------------------------------------------------------------------------*/
 
-var strRevealedColor           = '#367b36';
-var strRevealedBackgroundColor = '#ffffff';
+const strRevealedColor           = '#367b36';
+const strRevealedBackgroundColor = '#ffffff';
+
+const strHiddenColor             = '#e0e0e0';
+const strHiddenBackgroundColor   = '#e0e0e0';
+
+const strImagePrefix = '../../../../images/cards/';
+const strImageSuffix = '.svg';
 
 /*--------------------------------------------------------------------------------------------------*/
 
 function fncRevealWinRate(objElementAnswer)
 {
-  objElementAnswer.style.color           = strRevealedColor;
+  objElementAnswer.style.color = strRevealedColor;
   objElementAnswer.style.backgroundColor = strRevealedBackgroundColor;
+}
+
+/*--------------------------------------------------------------------------------------------------*/
+
+function fncGetRandomSuits(arrTemp, intNumberOfSuits) {
+  const arrSource = [...arrTemp];
+  const arrResult = [];
+
+  if (intNumberOfSuits > arrSource.length)
+  {
+    intNumberOfSuits = arrSource.length;
+  }strHiddenColor
+
+  for (let i = 0; i < intNumberOfSuits; i++)
+  {
+    const randomIndex = Math.floor(Math.random() * arrSource.length);
+    const selectedElement = arrSource[randomIndex];
+    arrResult.push(selectedElement);
+    arrSource.splice(randomIndex, 1);
+  }
+
+  return arrResult;
 }
 
 /*--------------------------------------------------------------------------------------------------*/
@@ -36,20 +66,37 @@ function fncDealCards()
   var objHoleCards = document.querySelector('meta[name="hole-cards"]');
   var strHoleCards = objHoleCards.getAttribute('content');
 
+  const arrTargetSuits = fncGetRandomSuits(arrSuits, 3);
+
   if (strHoleCards[0] === strHoleCards[1])
   {
     document.getElementById('idNameP').textContent = strHoleCards + 'p';
-    document.getElementById('idWinRateP').textContent = winRates[strHoleCards + 'p'];
+    document.getElementById('idWinRateP').textContent = arrWinRates[strHoleCards + 'p'];
+
+    document.getElementById('idWinRateP').style.color = strHiddenColor;
+    document.getElementById('idWinRateP').style.backgroundColor = strHiddenBackgroundColor;
+
+    document.getElementById('idCard1').setAttribute('src', strImagePrefix + strHoleCards[0] + arrTargetSuits[0] + strImageSuffix);
+    document.getElementById('idCard2').setAttribute('src', strImagePrefix + strHoleCards[1] + arrTargetSuits[0] + strImageSuffix);
   }
   else
   {
+    document.getElementById('idCard1').setAttribute('src', strImagePrefix + strHoleCards[0] + arrTargetSuits[0] + strImageSuffix);
+    document.getElementById('idCard2').setAttribute('src', strImagePrefix + strHoleCards[1] + arrTargetSuits[0] + strImageSuffix);
+    document.getElementById('idCard3').setAttribute('src', strImagePrefix + strHoleCards[0] + arrTargetSuits[1] + strImageSuffix);
+    document.getElementById('idCard4').setAttribute('src', strImagePrefix + strHoleCards[1] + arrTargetSuits[2] + strImageSuffix);
+
     document.getElementById('idNameS').textContent = strHoleCards + 's';
     document.getElementById('idNameO').textContent = strHoleCards + 'o';
-    document.getElementById('idWinRateS').textContent = winRates[strHoleCards + 's'];
-    document.getElementById('idWinRateO').textContent = winRates[strHoleCards + 'o'];
-  }
 
-  console.log();
+    document.getElementById('idWinRateS').textContent = arrWinRates[strHoleCards + 's'];
+    document.getElementById('idWinRateS').style.color = strHiddenColor;
+    document.getElementById('idWinRateS').style.backgroundColor = strHiddenBackgroundColor;
+
+    document.getElementById('idWinRateO').textContent = arrWinRates[strHoleCards + 'o'];
+    document.getElementById('idWinRateO').style.color = strHiddenColor;
+    document.getElementById('idWinRateO').style.backgroundColor = strHiddenBackgroundColor;
+  }
 }
 
 /*--------------------------------------------------------------------------------------------------*/
