9a3b1df1badaef111b5cba2984a09a04aae24b42
xenqos <169744738+xenqos@users.noreply.github.com>
2025-11-29 19:35:01 +0200

    11/29/25 19:34

---
+++ zones/linguistics/fr-phonemes-lettres/.preprocessing/move-files.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+#-----------------------------
+
+# mv ./archive/x007.txt ./texts/12.txt
+# mv ./archive/x048.txt ./texts/13.txt
+# mv ./archive/x006.txt ./texts/14.txt
+# mv ./archive/x008.txt ./texts/15.txt
+# mv ./archive/x019.txt ./texts/16.txt
+# mv ./archive/x073.txt ./texts/17.txt
+# mv ./archive/x024.txt ./texts/18.txt
+# mv ./archive/x062.txt ./texts/19.txt
+# mv ./archive/x038.txt ./texts/20.txt
+# mv ./archive/x046.txt ./texts/21.txt
+# mv ./archive/x310.txt ./texts/22.txt
+# mv ./archive/x025.txt ./texts/23.txt
+# mv ./archive/x026.txt ./texts/24.txt
+# mv ./archive/x295.txt ./texts/25.txt
+# mv ./archive/x042.txt ./texts/26.txt
+# mv ./archive/x035.txt ./texts/27.txt
+# mv ./archive/x037.txt ./texts/28.txt
+# mv ./archive/x018.txt ./texts/29.txt
+# mv ./archive/x030.txt ./texts/30.txt
+# mv ./archive/x012.txt ./texts/31.txt
+# mv ./archive/x017.txt ./texts/32.txt
+# mv ./archive/x014.txt ./texts/33.txt
+# mv ./archive/x128.txt ./texts/34.txt
+# mv ./archive/x031.txt ./texts/35.txt
+# mv ./archive/x013.txt ./texts/36.txt
+# mv ./archive/x064.txt ./texts/37.txt
+# mv ./archive/x051.txt ./texts/38.txt
+# mv ./archive/x065.txt ./texts/39.txt
+# mv ./archive/x015.txt ./texts/40.txt
+# mv ./archive/x173.txt ./texts/41.txt
+# mv ./archive/x229.txt ./texts/42.txt
+# mv ./archive/x451.txt ./texts/43.txt
+# mv ./archive/x129.txt ./texts/44.txt
+# mv ./archive/x022.txt ./texts/45.txt
+# mv ./archive/x029.txt ./texts/46.txt
+# mv ./archive/x016.txt ./texts/47.txt
+# mv ./archive/x028.txt ./texts/48.txt
+# mv ./archive/x027.txt ./texts/49.txt
+# mv ./archive/x044.txt ./texts/50.txt
+# mv ./archive/x070.txt ./texts/51.txt
+# mv ./archive/x072.txt ./texts/52.txt
+# mv ./archive/x324.txt ./texts/53.txt
+# mv ./archive/x081.txt ./texts/54.txt
+# mv ./archive/x082.txt ./texts/55.txt
+# mv ./archive/x074.txt ./texts/56.txt
+# mv ./archive/x075.txt ./texts/57.txt
+# mv ./archive/x094.txt ./texts/58.txt
+# mv ./archive/x032.txt ./texts/59.txt
+# mv ./archive/x033.txt ./texts/60.txt
+# mv ./archive/x045.txt ./texts/61.txt
+# mv ./archive/x036.txt ./texts/62.txt
+# mv ./archive/x068.txt ./texts/63.txt
+# mv ./archive/x047.txt ./texts/64.txt
+# mv ./archive/x126.txt ./texts/65.txt
+# mv ./archive/x069.txt ./texts/66.txt
+# mv ./archive/x041.txt ./texts/67.txt
+# mv ./archive/x096.txt ./texts/68.txt
+# mv ./archive/x055.txt ./texts/69.txt
+# mv ./archive/x056.txt ./texts/70.txt
+# mv ./archive/x067.txt ./texts/71.txt
+# mv ./archive/x100.txt ./texts/72.txt
+
+mv ./archive-texts/039.txt ./texts/73.txt
+mv ./archive-texts/040.txt ./texts/74.txt
+mv ./archive-texts/188.txt ./texts/75.txt
+mv ./archive-texts/196.txt ./texts/76.txt
+mv ./archive-texts/049.txt ./texts/77.txt
+mv ./archive-texts/052.txt ./texts/78.txt
+mv ./archive-texts/058.txt ./texts/79.txt
+mv ./archive-texts/060.txt ./texts/80.txt
+mv ./archive-texts/059.txt ./texts/81.txt
+mv ./archive-texts/061.txt ./texts/82.txt
+mv ./archive-texts/079.txt ./texts/83.txt
+mv ./archive-texts/080.txt ./texts/84.txt
+mv ./archive-texts/084.txt ./texts/85.txt
+mv ./archive-texts/050.txt ./texts/86.txt
+mv ./archive-texts/076.txt ./texts/87.txt
+mv ./archive-texts/085.txt ./texts/88.txt
+mv ./archive-texts/086.txt ./texts/89.txt
+mv ./archive-texts/087.txt ./texts/90.txt
+mv ./archive-texts/088.txt ./texts/91.txt
+mv ./archive-texts/089.txt ./texts/92.txt
+mv ./archive-texts/090.txt ./texts/93.txt
+mv ./archive-texts/105.txt ./texts/94.txt
+mv ./archive-texts/071.txt ./texts/95.txt
+mv ./archive-texts/078.txt ./texts/96.txt
+mv ./archive-texts/091.txt ./texts/97.txt
+mv ./archive-texts/105.txt ./texts/98.txt
+mv ./archive-texts/125.txt ./texts/99.txt
