61c6ad3d33bf45f078845e96f989182201fa3e34
xenqos <169744738+xenqos@users.noreply.github.com>
2026-01-10 13:29:39 +0200

    01/10/26 13:29

---
+++ zones/archive/linguistics/scripts/speech-convert.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#-------------------------------------------------------------------------------
+
+ext_src='mp3'
+ext_dst='opus'
+dir_name='sounds'
+bit_rate='24k'
+
+dir_prefix="${HOME}/mnt/data/Webpages/zones/linguistics/de-grammar"
+dir_src="${dir_prefix}/${dir_name}"
+dir_dst="${dir_prefix}/${dir_name}-${ext_dst}"
+
+#-------------------------------------------------------------------------------
+
+[[ ! -d "${dir_dst}" ]] && mkdir -p "${dir_dst}"
+
+for file in "${dir_src}"/*; do
+  file_noext="${file%.*}"
+  file_name="${file_noext##*/}"
+  ffmpeg -i "${file}" -c:a libopus -b:a "${bit_rate}" "${dir_dst}/${file_name}.${ext_dst}"
+done
+
+#-------------------------------------------------------------------------------
